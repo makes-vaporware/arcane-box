23529116ae3b77346af71233ef02652011622cde
steamed <aluminumcoins@gmail.com>
2025-03-19 04:25:49 +1100

    use hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { SpellViewer } from "./components/SpellViewer.jsx";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/arcane-box">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<SpellViewer />} />
         <Route path="/:spellName" element={<SpellViewer />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
